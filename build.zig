25d9a88c6184af4b53c1582eb8b8680744d4eadd
Ed Pizzella <ed.pizzella@gmail.com>
2024-12-08 12:31:04 -0500

    Package to module

--- build.zig
+++ build.zig
@@ -4,8 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addObject(.{
-        .name = "RTOS",
+    _ = b.addModule("RTOS", .{
         .root_source_file = b.path("os.zig"),
         .target = target,
         .optimize = optimize,
