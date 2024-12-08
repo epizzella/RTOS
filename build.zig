c78a9208ff55ed0b00624d1d8efe3ea7482be8af
Ed Pizzella <ed.pizzella@gmail.com>
2024-12-08 15:15:29 -0500

    Package

--- build.zig
+++ build.zig
@@ -1,8 +1,18 @@
 const std = @import("std");
+const cpu = std.Target.arm.cpu;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const cpu_model = target.query.cpu_arch;
+    if (cpu_model == &cpu.cortex_m0 or cpu_model == &cpu.cortex_m0plus) {
+        //include armv6m assembly
+    } else if (cpu_model == &cpu.cortex_m3 or cpu_model == &cpu.cortex_m4 or cpu_model == &cpu.cortex_m7) {
+        //include armv7m assembly
+    } else {
+        @compileError("Unsupported architecture selected.");
+    }
 
     _ = b.addModule("RTOS", .{
         .root_source_file = b.path("os.zig"),
