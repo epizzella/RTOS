8952cbde3794f1ab6adaabdb8283bd98d73c28bc
Ed Pizzella <ed.pizzella@gmail.com>
2024-12-08 11:51:50 -0500

    Package update

--- build.zig
+++ build.zig
@@ -4,12 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const RTOS = b.addObject(.{
+    _ = b.addObject(.{
         .name = "RTOS",
         .root_source_file = b.path("os.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    b.getInstallStep().dependOn(&b.addInstallArtifact(RTOS, .{ .dest_dir = .{ .override = .{ .custom = "" } } }).step);
 }
